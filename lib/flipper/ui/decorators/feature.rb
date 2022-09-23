899940cc91f46e15dee76f5ad67218d32f1fafb6
Chris VanHaren <vanharen@gmail.com>
2022-09-23 13:27:21 -0500

    bump version up and add 'Deisabled' field to status on features page

--- lib/flipper/ui/decorators/feature.rb
+++ lib/flipper/ui/decorators/feature.rb
@@ -52,6 +52,8 @@ module Flipper
             statuses << "#{feature.percentage_of_time_value}% of time"
           end
 
+          statuses << 'Disabled' if statuses.empty?
+
           Util.to_sentence(statuses)
         end
 
